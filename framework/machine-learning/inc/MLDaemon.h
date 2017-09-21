11c5a52e3f17a92b011f02d043713dc766375586
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-09-21 13:01:27 +0000

    Initial prototype of machine learning daemon

---
+++ framework/machine-learning/inc/MLDaemon.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2017 SKKU ESLAB, and contributors. All rights reserved.
+ *
+ * Contributor: Gyeonghwan Hong<redcarrottt@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __APP_CORE_H__
+#define __APP_CORE_H__
+
+#include "LocalChannel.h"
+#include "MessageRouter.h"
+#include "DbusChannel.h"
+#include "BaseMessage.h"
+
+#include <iostream>
+#include <vector>
+
+#define PATH_BUFFER_SIZE 1024
+
+class LocalChannel;
+
+class MLDaemon
+: public LocalChannelListener {
+
+  public:
+    MLDaemon() {
+    }
+
+    ~MLDaemon() {
+      delete this->mMessageRouter;
+      delete this->mDbusChannel;
+      delete this->mLocalChannel;
+    }
+
+    // Main loop
+    void run();
+
+    // LocalChannelListener
+    virtual void onReceivedMessage(BaseMessage* message);
+
+  protected:
+    // TODO: MLFW commands 
+
+    // Message framework
+    MessageRouter* mMessageRouter = NULL;
+    DbusChannel* mDbusChannel = NULL;
+    LocalChannel* mLocalChannel = NULL;
+};
+
+#endif // !defined(__APP_CORE_H__)
