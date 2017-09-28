8d7a50de334f5cc49bb2f4f39fdd5bcb60ec3c65
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-09-28 12:44:00 +0000

    Add mutex locks to InferenceUnit of Machine Learning FW

--- framework/machine-learning/inc/InferenceUnit.h
+++ framework/machine-learning/inc/InferenceUnit.h
@@ -64,7 +64,8 @@ class InferenceUnit {
     static void* inferenceLoop(void*);
 
     // Commands
-    bool run();
+    bool start();
+    bool stop();
     bool setInput(std::string inputName, std::string sourceUri);
     bool startListeningOutput(std::string listenerUri);
     bool stopListeningOutput(std::string listenerUri);
@@ -99,7 +100,10 @@ class InferenceUnit {
     InferenceUnit(int iuid, std::string modelPackagePath) 
     : mIuid(iuid), mState(InferenceUnitState::Initialized),
     mModelPackagePath(modelPackagePath), mPid(-1),
-    mFieldsMutex(PTHREAD_MUTEX_INITIALIZER) {
+    mThreadRunningMutex(PTHREAD_MUTEX_INITIALIZER),
+    mInputMutex(PTHREAD_MUTEX_INITIALIZER),
+    mOutputMutex(PTHREAD_MUTEX_INITIALIZER),
+    mIsThreadRunning(false) {
       // NOTICE: Its child classes should create its own InferenceRunner
       //         in the child's constructor function.
       this->mInputReaderSet = new InputReaderSet();
@@ -151,9 +155,12 @@ class InferenceUnit {
     InferenceRunner* mInferenceRunner = NULL;
 
     // Inference Unit Thread //
+    bool mIsThreadRunning;
     pthread_t mInferenceUnitThread;
 
-    pthread_mutex_t mFieldsMutex;
+    pthread_mutex_t mThreadRunningMutex;
+    pthread_mutex_t mInputMutex;
+    pthread_mutex_t mOutputMutex;
 };
 
 #endif // !defined(__INFERENCE_UNIT_H__)
