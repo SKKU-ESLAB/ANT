a305a2f5e08118375bb853f48d9e25681c8bb6a7
Hayun Lee <lhy920806@gmail.com>
2017-10-28 07:52:30 +0000

    Minimize the install-deps-*.sh execution time
    * Add Apache LICENSE for Camera Framework
    * Remove face detection API (temporary)
    * Fix raspberry pi camera-config.json
    * Remove unused GStreamer packages
    * Utilize multi-core (in prerequisite installation time)

--- target/raspberry-pi2_3/install-deps-raspberry-pi2_3.sh
+++ target/raspberry-pi2_3/install-deps-raspberry-pi2_3.sh
@@ -3,15 +3,15 @@
 ##########################################################################
 #
 # Copyright (c) 2017 SKKU ESLAB, and contributors. All rights reserved.
-# 
+#
 # Contributor: Gyeonghwan Hong<redcarrottt@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,20 +33,20 @@ print_progress() {
   WARN_COLO="\033[31;47m"
   INFO_COLO="\033[36m"
   INIT_COLO="\033[0m"
-  
+
   echo -e "${INFO_COLO}Step ${STEP_NUM}. ${STEP_DESC} ${INIT_COLO}"
 }
 
 # Step 1. Install packages by apt-get
 print_progress 1 "Install dependent packages..."
 sudo apt-get update
-sudo apt-get -y install g++-4.8                                               \
-  wiringpi libdbus-1-dev glib-2.0 libdbus-glib-1-2 bison yacc                 \
-  libdbus-glib-1-2-dbg libdbus-glib-1-dev zip sqlite3 libsqlite3-dev cmake    \
-  libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev git  \
-  python-dev python-numpy libjpeg-dev libpng-dev libtiff-dev libjasper-dev    \
-  libdc1394-22-dev automake libtool libssl-dev libnl-3-dev libnl-genl-3-dev   \
-  python3 udhcpd libv4l-dev libboost-dev libboost-serialization-dev gstreamer1.0
+sudo apt-get -y install g++-4.8 wiringpi libdbus-1-dev glib-2.0 bison byacc   \
+  libdbus-glib-1-2 libdbus-glib-1-dev zip sqlite3 libsqlite3-dev cmake git    \
+  libgtk2.0-dev pkg-config automake libtool libssl-dev libnl-3-dev python3    \
+  libnl-genl-3-dev udhcpd libopencv-dev libxml2-dev curl                      \
+  libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good     \
+  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav       \
+  gstreamer1.0-doc gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
 
 # Get the absolute path of ANT repository directory
 ANT_REPO_DIR=$(dirname "$0")/../..
@@ -59,13 +59,11 @@ git submodule update --init --recursive
 
 # Step 3. Build and reinstall bluez-4.101
 print_progress 3 "Build and reinstall bluez-4.101..."
-# Remove existing bluez
-sudo apt-get remove bluez
 # Install bluez
 cd ${ANT_REPO_DIR}/dep/bluez-4.101
 ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc \
-  --localstatedir=/var --libexecdir=/lib 
-make 
+  --localstatedir=/var --libexecdir=/lib
+make -j4
 sudo make install
 sudo systemctl unmask bluetooth
 sudo service bluetooth start
@@ -74,43 +72,26 @@ sudo service bluetooth start
 print_progress 4 "Set udhcpd config..."
 sudo touch /var/lib/misc/udhcpd.leases
 
-# Step 5. Build and install libxml2-2.9.4-rc2
-print_progress 5 "Build and install libxml2-2.9.4-rc2..."
-cd ${ANT_REPO_DIR}/dep/libxml2-2.9.4-rc2
-./autogen.sh
-./configure --prefix=/usr/local/xml
-make
-sudo make install
+# Step 5. Link include directory for libxml2
+print_progress 5 "Link include directory for libxml2..."
+sudo ln -s /usr/include/libxml2/libxml /usr/include/libxml
 
-# Step 6. Build and install opencv-3.0.0
-print_progress 6 "Build and install opencv-3.0.0..."
-cd /usr/include/linux
-sudo ln -s ../libv4l1-videodev.h videodev.h
-cd ${ANT_REPO_DIR}/dep/opencv-3.0.0
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D \
-  BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF ..
-make -j 4
-sudo make install
-
-# Step 7. Build and install libuv-v1.7.5
-print_progress 7 "Build and install libuv-v1.7.5..."
+# Step 6. Build and install libuv-v1.7.5
+print_progress 6 "Build and install libuv-v1.7.5..."
 cd ${ANT_REPO_DIR}/dep/libuv-v1.7.5
 sh autogen.sh
 ./configure
-make
-make check
+make -j4
 sudo make install
 
-# Step 8. Copy dbus config file
-print_progress 8 "Copy dbus config file for ANT..."
+# Step 7. Copy dbus config file
+print_progress 7 "Copy dbus config file for ANT..."
 sudo cp ${ANT_REPO_DIR}/dep/ant-dbus-config/ant.conf /etc/dbus-1/system.d/ant.conf
 
-# Step 9. Install wpa_supplicant, wpa_cli and deletesem
-print_progress 9 "Install wpa_supplicant, wpa_cli and deletesem..."
+# Step 8. Install wpa_supplicant, wpa_cli and deletesem
+print_progress 8 "Install wpa_supplicant, wpa_cli and deletesem..."
 cd ${ANT_REPO_DIR}/dep/hostap/wpa_supplicant
-make
+make -j4
 cd ${ANT_REPO_DIR}/dep/deletesem
 gcc -o deletesem deletesem.c -lpthread
 
@@ -120,22 +101,21 @@ sudo cp ${ANT_REPO_DIR}/dep/hostap/wpa_supplicant/wpa_cli /usr/bin/ant-deps/
 sudo cp ${ANT_REPO_DIR}/dep/deletesem/deletesem /usr/bin/ant-deps/
 sudo chmod +x /usr/bin/ant-deps/*
 
-# Step 10. Build and install nodejs-4.0.0
-print_progress 10 "Build and install nodejs-4.0.0..."
-cd ${ANT_REPO_DIR}/dep/nodejs-4.0.0
-./configure
-make -j4
-sudo make install
+# Step 9. Install nodejs-4.x package
+print_progress 9 "Install nodejs-4.x package..."
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get -y install nodejs
 
-# Step 11. Install nan, node-gyp package 
+# Step 10. Install nan, node-gyp package
+print_progress 10 "Install nan, node-gyp package..."
 cd ${ANT_REPO_DIR}
 npm install nan
 sudo npm install -g node-gyp
 
-# Step 12. Install Gstreamer RPI camera source element
+# Step 11. Install Gstreamer RPI camera source element
 cd ${ANT_REPO_DIR}/dep/gst-rpicamsrc
 ./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/
-make
+make -j4
 sudo make install
 
 WARN_COLO="\033[31;47m"
