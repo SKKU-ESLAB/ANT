5f2964caa7e02b7f4fcca11fb37739d1494ca537
Gyeonghwan Hong <RedCarrottt@gmail.com>
2018-08-10 21:28:00 +0900

    Relocate README.md and add auxiliary scripts

--- linux/script/wfd_off.sh
+++ linux/script/wfd_off.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-then echo "$0: it needs privilege permission."
-  exit
-fi
 
-wpa_cli -i wlan0 p2p_group_remove p2p-wlan0-$1
+sudo wpa_cli -i wlan0 p2p_group_remove p2p-wlan0-$1
+sudo ifconfig wlan0 down
