1305186dc277311fc06e847cbe4113c49aaa7dbb
Gyeonghwan Hong <RedCarrottt@gmail.com>
2018-12-05 18:56:50 +0900

    Introduce ANT Framework configuration * Introduce ANT Framework configuration * Deprecate legacy communication API   - It has already been replaced by new communication API     in 'new-api' directory. * Deprecate target device config via cmake argument

---
+++ kconfig.cmake
@@ -0,0 +1,34 @@
+# Original Source: https://github.com/zephyrproject-rtos/zephyr (Apache 2.0)
+# 2.2 Misc
+#
+# Parse a KConfig formatted file (typically named *.config) and
+# introduce all the CONF_ variables into the CMake namespace
+function(import_kconfig config_file)
+  # Parse the lines prefixed with CONFIG_ in ${config_file}
+  file(
+    STRINGS
+    ${config_file}
+    DOT_CONFIG_LIST
+    REGEX "^CONFIG_"
+    ENCODING "UTF-8"
+    )
+
+  foreach (CONFIG ${DOT_CONFIG_LIST})
+    # CONFIG looks like: CONFIG_NET_BUF=y
+
+    # Match the first part, the variable name
+    string(REGEX MATCH "[^=]+" CONF_VARIABLE_NAME ${CONFIG})
+
+    # Match the second part, variable value
+    string(REGEX MATCH "=(.+$)" CONF_VARIABLE_VALUE ${CONFIG})
+    # The variable name match we just did included the '=' symbol. To just get the
+    # part on the RHS we use match group 1
+    set(CONF_VARIABLE_VALUE ${CMAKE_MATCH_1})
+
+    if("${CONF_VARIABLE_VALUE}" MATCHES "^\"(.*)\"$") # Is surrounded by quotes
+      set(CONF_VARIABLE_VALUE ${CMAKE_MATCH_1})
+    endif()
+
+    set("${CONF_VARIABLE_NAME}" "${CONF_VARIABLE_VALUE}" PARENT_SCOPE)
+  endforeach()
+endfunction()
