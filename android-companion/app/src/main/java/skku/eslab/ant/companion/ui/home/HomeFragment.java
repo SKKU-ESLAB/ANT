bf58f6ee1503f94ef64df08f5444222bb7b9b591
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-11-25 02:08:41 +0900

    Prototype of home fragment in Companion Lite app

--- android-companion/app/src/main/java/skku/eslab/ant/companion/ui/home/HomeFragment.java
+++ android-companion/app/src/main/java/skku/eslab/ant/companion/ui/home/HomeFragment.java
@@ -20,12 +20,7 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                //
-            }
-        });
+
         return root;
     }
 }
