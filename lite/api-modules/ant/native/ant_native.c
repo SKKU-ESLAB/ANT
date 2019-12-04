2aa069a0b6320ce28723bc710efa4e03a2537692
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-12-04 22:33:16 +0900

    Implement JS part of Stream API

--- lite/api-modules/ant/native/ant_native.c
+++ lite/api-modules/ant/native/ant_native.c
@@ -2,48 +2,37 @@
 
 #include "iotjs_def.h"
 
-JS_FUNCTION(ant_stream_testPipeline) {
-  iotjs_string_t argIpAddress;
-  bool result;
+#define ANT_API_STRING_TO_STRING(api_name, function_name)                      \
+  JS_FUNCTION(ant_##api_name##_##function_name) {                              \
+    iotjs_string_t argMessage;                                                 \
+    char result_string[100];                                                   \
+    DJS_CHECK_ARGS(1, string);                                                 \
+    argMessage = JS_GET_ARG(0, string);                                        \
+    ant_##api_name##_##function_name##_internal(                               \
+        iotjs_string_data(&argMessage), result_string);                        \
+    iotjs_string_destroy(&argMessage);                                         \
+    return jerry_create_string((const jerry_char_t *)result_string);           \
+  }
 
-  // Function input arguments
-  DJS_CHECK_ARGS(1, string);
-  argIpAddress = JS_GET_ARG(0, string);
+#define ANT_API_VOID_TO_VOID(api_name, function_name)                          \
+  JS_FUNCTION(ant_##api_name##_##function_name) {                              \
+    ant_##api_name##_##function_name##_internal();                             \
+    return jerry_create_undefined());                                          \
+  }
 
-  // Internal native function call
-  result = ant_stream_testPipeline_internal(iotjs_string_data(&argIpAddress));
+#define REGISTER_ANT_API(module, api_name, function_name)                      \
+  iotjs_jval_set_method(##module##, "##api_name##_##function_name##",          \
+                        ant_##api_name##_##function_name##);
 
-  // Destroy iotjs objects
-  iotjs_string_destroy(&argIpAddress);
-  return jerry_create_boolean(result);
-}
-
-JS_FUNCTION(ant_stream_callDbusMethod) {
-  iotjs_string_t argMessage;
-  char result_string[100];
-
-  // Function input arguments
-  DJS_CHECK_ARGS(1, string);
-  argMessage = JS_GET_ARG(0, string);
-
-  // Internal native function call
-  ant_stream_callDbusMethod_internal(iotjs_string_data(&argMessage),
-                                     result_string);
-
-  // Destroy iotjs objects
-  iotjs_string_destroy(&argMessage);
-  return jerry_create_string((const jerry_char_t *)result_string);
-}
+ANT_API_VOID_TO_VOID(stream, initializeStream);
+ANT_API_STRING_TO_STRING(stream, callDbusMethod);
 
 jerry_value_t InitANTNative() {
-  jerry_value_t mymodule = jerry_create_object();
-  // iotjs_jval_set_property_string_raw(mymodule, "message", "Hello world!");
-  iotjs_jval_set_method(mymodule, "stream_testPipeline",
-                        ant_stream_testPipeline);
-  iotjs_jval_set_method(mymodule, "stream_callDbusMethod",
-                        ant_stream_callDbusMethod);
+  jerry_value_t antNative = jerry_create_object();
+  REGISTER_ANT_API(antNative, stream, initializeStream);
+  REGISTER_ANT_API(antNative, stream, callDbusMethod);
 
   initANTStream();
 
-  return mymodule;
+  return antNative;
 }
