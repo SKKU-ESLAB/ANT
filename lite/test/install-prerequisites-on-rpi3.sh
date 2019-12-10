51713d3a9ece03f0ee729f172d1a780582a900ad
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-12-10 21:11:41 +0900

    Add dependent packages for TVM and nnstreamer

--- lite/test/install-prerequisites-on-rpi3.sh
+++ lite/test/install-prerequisites-on-rpi3.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo apt-get install cmake git libdbus-1-dev glib-2.0 libdbus-glib-1-2 libdbus-glib-1-dev \
   libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
-  gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
+  gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer-plugins-base1.0-dev \
+  gstreamer1.0-doc gstreamer1.0-tools libtinfo-dev libprotobuf-dev
 
 # Build IoT.js
 cd ../../dep/iotjs
