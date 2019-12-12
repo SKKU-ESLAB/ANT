6aff8b5626b7d97ecdf57fc753b6980873fca2ff
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-12-12 18:21:02 +0900

    Support for Ubuntu Mate on Raspberry Pi 3

---
+++ lite/examples/stream_api_streaming_test_example2.js
@@ -0,0 +1,55 @@
+var ant = require('ant');
+var console = require('console');
+
+var settings = {};
+settings.video_width = 480;
+settings.video_height = 360;
+settings.video_framerate = "30/1";
+settings.video_format = "RGB";
+settings.video_sink_sync = false;
+settings.my_ip_address = "192.168.0.33";
+settings.my_port = 5000;
+
+var on_initialize = function () {
+  console.log('on_initialize');
+};
+
+var on_start = function () {
+  console.log('on_start');
+
+  // Because ant.stream.initialize() is an async function without finish callback,
+  // pipeline setting should be executed by setTimeout.
+  ant.stream.initialize();
+  setTimeout(function () {
+    var pipeline = ant.stream.createPipeline("test");
+
+    var source = ant.stream.createElement("videotestsrc");
+    source.setProperty("pattern", 1);
+    var filter = ant.stream.createElement("capsfilter");
+    filter.setCapsProperty("caps", "video/x-raw,width=" + settings.video_width
+      + ",height=" + settings.video_height + ",framerate=" + settings.video_framerate
+      + ",format=" + settings.video_format);
+    var converter = ant.stream.createElement("videoconvert");
+    var gdppay = ant.stream.createElement("gdppay");
+    var sink = ant.stream.createElement("tcpserversink");
+    sink.setProperty("sync", settings.video_sink_sync);
+    sink.setProperty("host", settings.my_ip_address);
+    sink.setProperty("port", settings.my_port);
+
+    pipeline.binAdd([source, filter, converter, gdppay, sink]);
+    pipeline.linkMany([source, filter, converter, gdppay, sink]);
+    pipeline.setState(pipeline.STATE_PLAYING);
+    console.log("Pipeline ready! (" + settings.my_ip_address + ":" + settings.my_port + ")");
+
+    var remote_pipeline = "tcpclientsrc host=" + settings.my_ip_address + " port=" + settings.my_port
+      + " ! gdpdepay ! videoconvert ! autovideosink sync=false";
+    ant.remoteui.setStreamingViewPipeline(remote_pipeline);
+  }, 2000);
+};
+
+var on_stop = function () {
+  console.log('on_stop');
+  ant.stream.finalize();
+};
+
+ant.runtime.setCurrentApp(on_initialize, on_start, on_stop);
--- lite/test/install-prerequisites-on-rpi3.sh
+++ lite/test/install-prerequisites-on-rpi3.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # ANT dependenies
 sudo apt-get update
-sudo apt-get install -y cmake git libdbus-1-dev glib-2.0 libdbus-glib-1-2 libdbus-glib-1-dev
-sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
-  gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav \
-  libgstreamer-plugins-base1.0-dev gstreamer1.0-doc gstreamer1.0-tools
+sudo apt-get install -y cmake git
+sudo apt-get install -y libdbus-1-dev glib-2.0 libdbus-glib-1-2 libdbus-glib-1-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-doc gstreamer1.0-tools \
+  gstreamer1.0-plugins-base libgstreamer-plugins-base1.0-dev \
+  gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav gstreamer-omx
 
 # TVM dependencies
 sudo apt-get install -y python3 python3-dev python3-pip python3-setuptools gcc \
-  libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev libtinfo-dev
-#sudo apt-get install -y libprotobuf-dev
-#sudo apt-get install -y python3 python3-decorator libjasper libqtgui4 libqt4-test libatlas-base-dev
-#pip3 install opencv-contrib-python==4.1.0.25
+  libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev libtinfo-dev \
+  python3-opencv
+cd ../dep/tvm
 
 # Build IoT.js
 cd ../../dep/iotjs
