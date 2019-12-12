7fc211d9279aedbbd6643d3e6734bf52891435f6
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-12-12 18:41:14 +0900

    Add ANT-NN(TVM-NNStreamer)

--- lite/test/build-iotjs-module-on-rpi3.sh
+++ lite/test/build-iotjs-module-on-rpi3.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# Build ANT API and App Runtime for IoT.js
 MODULE_PATH=../../lite/api-modules/
 cd ../../dep/iotjs/
 ./tools/build.py --target-board=rpi3 --cmake-param=-DENABLE_MODULE_ANT=ON \
     --external-modules=${MODULE_PATH}/ant
 cd -
 
+# Install ANT API and App Runtime for IoT.js
 mkdir -p iotjs
 cd iotjs
 cp ../../../dep/iotjs/build/arm-linux/debug/bin/iotjs ./
 cp -r ../../app-runtime/* ./
+
+# Install ANT-NN
+cp -r ../../nn ./
