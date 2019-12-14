8c8a5dcde90e4e2b27d740779da43c0e60df3aaf
Gyeonghwan Hong <redcarrottt@gmail.com>
2019-12-14 15:16:28 +0900

    Add dependency to TVM

--- lite/test/install-prerequisites-on-rpi3.sh
+++ lite/test/install-prerequisites-on-rpi3.sh
@@ -9,20 +9,22 @@ sudo apt-get install -y libraspberrypi-dev
 sudo apt-get install -y python3 python3-dev python3-pip python3-setuptools gcc \
   libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev libtinfo-dev \
   python3-opencv
-cd ../dep/tvm
+cd ../../dep/tvm
+make runtime -j2
+echo "export PYTHONPATH=$PWD/python:$PYTHONPATH" >> ~/.bashrc
 
 # Build IoT.js
 cd ../../dep/iotjs
 ./tools/build.py --target-board=rpi3
 
-# Gstreamer
+# Install Gstreamer and its elements
 sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-doc gstreamer1.0-tools \
   gstreamer1.0-plugins-base libgstreamer-plugins-base1.0-dev \
   gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
   gstreamer1.0-libav
 sudo chmod 777 /dev/vchiq
 
-# nnstreamer
+# Install nnstreamer
 sudo apt-add-repository -y ppa:nnstreamer
 sudo apt-get update
 sudo apt-get install -y nnstreamer-python3 nnstreamer-dev
