e1fd2bf76bfa30876fd40c8856fa2bdc23dbedce
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-09-25 02:42:44 +0900

    Prototype of OCFResource APIs

--- api/ocf/js/ocf.js
+++ api/ocf/js/ocf.js
@@ -14,9 +14,14 @@ OCF.prototype.getAdapter = function () {
   return sOCFAdapter;
 };
 
+OCF.prototype.createResource = function(name, uri, types, interface_mask) {
+  return new OCFResource(name, uri, types, interface_mask);
+};
+
 function OCFAdapter() {
   this._mfg_name = "";
   this._devices = [];
+  this._resources = [];
 
   // Default handler
   var self = this;
@@ -50,13 +55,19 @@ OCFAdapter.prototype.onPrepareServer = function (handler) {
   native.ocf_adapter_onPrepareServer(handler);
 };
 OCFAdapter.prototype.start = function () {
-  native.ocf_adapter_start(this);
+  native.ocf_adapter_start();
 };
 OCFAdapter.prototype.stop = function () {
-  native.ocf_adapter_stop(this);
+  native.ocf_adapter_stop();
 };
 
 OCFAdapter.prototype.addResource = function (resource) {
+  this._resources.push(resource);
+  // TODO:
+  native.ocf_adapter_addResource(resource.name, resource.uri, resource.types, resource.interface_mask);
+};
+OCFAdapter.prototype.getResources = function () {
+  return this._resources;
 };
 
 function OCFDevice(uri, resource_type, name, spec_version, data_model_version) {
@@ -67,10 +78,33 @@ function OCFDevice(uri, resource_type, name, spec_version, data_model_version) {
   this.data_model_version = data_model_version;
 }
 
-function OCFResource() { }
+function OCFResource(name, uri, types, interface_mask) {
+  this.name = name;
+  this.uri = uri;
+  this.types = types;
+  this.interface_mask = interface_mask;
+  this.is_discoverable = false;
+  this.periodic_observable = 
+
+  // TODO: binding with native ocf_resource_t
+  native.ocf_resource_constructor(name, uri, types, interface_mask);
+}
 
 OCFResource.prototype.setHandler = function (type, handler) {
+  // TODO:
+  native.ocf_resource_setHandler(this, type, handler);
+};
 
+OCFResource.prototype.setDiscoverable = function (is_discoverable) {
+  this.is_discoverable = is_discoverable;
+  // TODO:
+  native.ocf_resource_setDiscoverable(this, is_discoverable);
+};
+
+OCFResource.prototype.setPeriodicObservable = function (periodic_b) {
+  this.is_discoverable = is_discoverable;
+  // TODO:
+  native.ocf_resource_setPeriodicObservable(this, is_periodicObservable);
 };
 
 module.exports = new OCF();
