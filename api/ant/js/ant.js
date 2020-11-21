29258c36558dda8ed526e601f3480b0c74ff0b54
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-11-21 23:48:40 +0900

    Add JavaScript code style checker to CI system

--- api/ant/js/ant.js
+++ api/ant/js/ant.js
@@ -3,37 +3,37 @@ var console = require('console');
 /** ANT main object start **/
 function ANT() { }
 
-console.log("ANT API checking...");
+console.log('ANT API checking...');
 
 try {
   ANT.prototype.runtime = require('antruntime');
 } catch (e) {
-  throw new Error("ANT Runtime API is always required!");
+  throw new Error('ANT Runtime API is always required!');
 }
 try {
   ANT.prototype.resource = require('antresource');
 } catch (e) {
-  console.log("> Not found Resource API");
+  console.log('> Not found Resource API');
 }
 try {
   ANT.prototype.remoteui = require('antremoteui');
 } catch (e) {
-  console.log("> Not found Remote UI API");
+  console.log('> Not found Remote UI API');
 }
 try {
   ANT.prototype.stream = require('antstream');
 } catch (e) {
-  console.log("> Not found Stream API");
+  console.log('> Not found Stream API');
 }
 try {
   ANT.prototype.ml = require('antml');
 } catch (e) {
-  console.log("> Not found ML API");
+  console.log('> Not found ML API');
 }
 try {
   ANT.prototype.companion = require('antcompanion');
 } catch (e) {
-  console.log("> Not found Companion API");
+  console.log('> Not found Companion API');
 }
 
 /** ANT main object end **/
