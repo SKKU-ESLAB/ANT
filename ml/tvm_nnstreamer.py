25a6890d3a0d53f9d4a489d3278815be720e29c4
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-11-21 21:48:28 +0900

    Fix code style of tvm_nnstreamer

--- ml/tvm_nnstreamer.py
+++ ml/tvm_nnstreamer.py
@@ -1,12 +1,8 @@
 import tvm
 from tvm.contrib import graph_runtime
-import time
 
 import nnstreamer_python as nns
-
 import numpy as np
-import cv2
-
 
 def shape_str_to_npshape(shape_str):
     shape_str_tokens = shape_str.split(":")
@@ -14,15 +10,14 @@ def shape_str_to_npshape(shape_str):
 
 
 def datatype_str_to_nptype(datatype_str):
+    ret = None
     if datatype_str == "float32":
-        return np.float32
+        ret = np.float32
     elif datatype_str == "int32":
-        return np.int32
+        ret = np.int32
     elif datatype_str == "uint8":
-        return np.uint8
-    else:
-        return None
-
+        ret = np.uint8
+    return ret
 
 class CustomFilter(object):
     def __init__(self, *args):
@@ -35,10 +30,10 @@ class CustomFilter(object):
         output_shape = shape_str_to_npshape(args[3])
         output_type = datatype_str_to_nptype(args[4])
 
-        if input_type == None:
+        if input_type is None:
             print("Invalid input_shape")
             return None
-        elif output_type == None:
+        if output_type is None:
             print("Invalid output_shape")
             return None
         self.input_dims = [nns.TensorShape(input_shape, input_type)]
@@ -51,34 +46,23 @@ class CustomFilter(object):
 
         self.module = graph_runtime.create(loaded_json, loaded_lib, ctx)
         self.module.load_params(loaded_params)
+        return None
 
     def getInputDim(self):
+        # pylint: disable=invalid-name
         return self.input_dims
 
     def getOutputDim(self):
+        # pylint: disable=invalid-name
         return self.output_dims
 
     def invoke(self, input_array):
-        preprocess_start_ts = time.time()
-
         input_tensor = np.reshape(
             input_array[0], self.input_dims[0].getDims()[::-1])[0]
-        image = input_tensor
-
         input_image = transform_image(input_tensor)
 
-        process_start_ts = time.time()
-
         self.module.run(data=input_image)
-
         out = self.module.get_output(0)
-
-        process_end_ts = time.time()
-
-        # print("Preprocessing(reshape+transform): %f ms / Inference(TVM): %f ms"
-        #  % ((process_start_ts - preprocess_start_ts) / 1000,
-        #     (process_end_ts - process_start_ts) / 1000))
-
         return [out.asnumpy().astype(np.float32)]
 
 
