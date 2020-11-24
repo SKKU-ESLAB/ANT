0b5c90d8a6fe3e20f097384b4485ebdf3601e84f
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-11-24 14:26:13 +0900

    Add license descriptions on each file's head part

--- api/antcompanion/native/ant_companion_native.c
+++ api/antcompanion/native/ant_companion_native.c
@@ -1,5 +1,20 @@
-#include "internal/ant_companion_native_internal.h"
+/* Copyright (c) 2017-2020 SKKU ESLAB, and contributors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "../../common/native/ant_common.h"
+#include "internal/ant_companion_native_internal.h"
 
 #include "iotjs_def.h"
 #include "iotjs_uv_request.h"
