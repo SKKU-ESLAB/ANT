bf806f11bd714c7a4f1a9496d61f7d13dac3ae88
YJ Lee <ls4154.lee@gmail.com>
2020-11-28 15:31:51 +0900

    Update CameraViewerActivity license

--- android-companion/app/src/main/java/com/ant/ant_manager/view/CameraViewerActivity.java
+++ android-companion/app/src/main/java/com/ant/ant_manager/view/CameraViewerActivity.java
@@ -1,9 +1,6 @@
 package com.ant.ant_manager.view;
 
-/* Copyright (c) 2017 SKKU ESLAB, and contributors. All rights reserved.
- *
- * Contributor: Gyeonghwan Hong<redcarrottt@gmail.com>
- *              Dongig Sin<dongig@skku.edu>
+/* Copyright (c) 2017-2020 SKKU ESLAB, and contributors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
