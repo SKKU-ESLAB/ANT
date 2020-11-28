6948691f30154e1e0c230b5483c2f2b30f4fbf8f
minusjet <scvhero9607@gmail.com>
2020-11-28 15:32:24 +0900

    update license

--- android-companion/app/src/main/java/skku/eslab/ant/companion/resourceapi/ResourceAPI.java
+++ android-companion/app/src/main/java/skku/eslab/ant/companion/resourceapi/ResourceAPI.java
@@ -7,9 +7,7 @@ import java.util.HashMap;
 import skku.eslab.ant.companion.companionapi.CompanionAPI;
 import skku.eslab.ant.companion.companionapi.OnReceiveMessageListener;
 
-/* Copyright (c) 2019, contributors. All rights reserved.
- *
- * Contributor: Gyeonghwan Hong <redcarrottt@gmail.com>
+/* Copyright (c) 2017-2020 SKKU ESLAB, and contributors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
