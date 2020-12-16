6592fe8178c318c51262828cd043adb066966f1c
Gyeonghwan Hong <redcarrottt@gmail.com>
2020-12-16 06:28:09 +0000

    Deprecate download_sample_model.sh script

--- ml/download_sample_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DL_ROOT=https://raw.githubusercontent.com/SKKU-ESLAB/ant-sample-ml-models/master/rpi3_mobilenet_full/
-mkdir -p sample-models/rpi3_mobilenet_full
-cd sample-models/rpi3_mobilenet_full
-wget ${DL_ROOT}/deploy_graph.json
-wget ${DL_ROOT}/deploy_lib.tar
-wget ${DL_ROOT}/deploy_lib.tar.so
-wget ${DL_ROOT}/deploy_param.params
-wget ${DL_ROOT}/imagenet-simple-labels.json
