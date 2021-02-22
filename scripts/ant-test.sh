4d242662ad371e73a006b5c0896c9b0c80ce3d6a
Gyeonghwan Hong <redcarrottt@gmail.com>
2021-02-22 11:14:55 +0900

    Update ant-test.sh

--- scripts/ant-test.sh
+++ scripts/ant-test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-export ANT_ROOT="$( cd "$( dirname "$0" )" && pwd -P )"
-echo "ANT_ROOT: $ANT_ROOT"
-if [ $# -ne 2 ]; then
-  echo "usage: $1 [app name] [--run or --debug]"
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+  echo "usage: $1 [app name] [--run or --debug] [(optional) APP_ROOT]"
   exit -1
 fi
 
+export ANT_ROOT="$( cd "$( dirname "$0" )" && pwd -P )"
+if [ $# -eq 3 ]; then
+  export APP_ROOT=$3
+else
+  export APP_ROOT="$ANT_ROOT/apps"
+fi
+echo "ANT_ROOT: $ANT_ROOT"
+
 if [ $2 == "--run" ]; then
-  ${ANT_ROOT}/iotjs ${ANT_ROOT}/app-core/main.js ${1} ${ANT_ROOT}/apps/${1}.js 0
+  ${ANT_ROOT}/iotjs ${ANT_ROOT}/app-core/main.js ${1} ${APP_ROOT}/${1}.js 0
 elif [ $2 == "--debug" ]; then
-  gdb --args ${ANT_ROOT}/iotjs ${ANT_ROOT}/app-core/main.js ${1} ${ANT_ROOT}/apps/${1}.js 0
+  gdb --args ${ANT_ROOT}/iotjs ${ANT_ROOT}/app-core/main.js ${1} ${APP_ROOT}/${1}.js 0
 fi
