0ff24a8b184f491cd28f3153e7b50630af7a5a37
Hayun Lee <lhy920806@gmail.com>
2021-05-11 15:43:33 +0900

    Add runtime python API

---
+++ api/python/runtime.py
@@ -0,0 +1,11 @@
+def ANTRuntime():
+    raise NotImplemented
+
+def App(on_initialize, on_start, on_stop):
+    """
+    Args:
+        on_initialize (Function): Callback handler invoked on application's initialization.
+        on_start (Function): Callback handler invoked when application starts.
+        on_stop (Function): Callback handler invoked when application stops.
+    """
+    raise NotImplemented
