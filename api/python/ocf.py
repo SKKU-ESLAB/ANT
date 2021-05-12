0045187e2fb491d8fe25dfda0257c29593017330
Hayun Lee <lhy920806@gmail.com>
2021-05-12 14:43:12 +0900

    Add OCF python API

---
+++ api/python/ocf.py
@@ -0,0 +1,11 @@
+def OCF():
+    raise NotImplemented
+
+def OCFAdapter():
+    raise NotImplemented
+
+def OCFDevice(id, uri, resource_type, name, spec_version, data_model_version):
+    raise NotImplemented
+
+def OCFResource(device, name, uri, types, interface_masks):
+    raise NotImplemented
