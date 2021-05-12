5b4c7bdd32224854e86525500fbc0f02616439c0
Hayun Lee <lhy920806@gmail.com>
2021-05-12 17:01:06 +0900

    Add Gateway python API

---
+++ api/python/gateway.py
@@ -0,0 +1,20 @@
+class ANTGateway():
+    raise NotImplemented
+
+class VirtualSensorAdapter():
+    raise NotImplemented
+
+class VirtualSensor():
+    raise NotImplemented
+
+class GatewayManager():
+    raise NotImplemented
+
+class VirtualSensorManager():
+    raise NotImplemented
+
+class DFEScheduler():
+    raise NotImplemented
+
+class GatewayClient():
+    raise NotImplemented
